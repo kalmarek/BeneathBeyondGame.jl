0ecf874283fce3fb94a5c1a4e79f792ed8c15c1e
kalmarek <kalmar@amu.edu.pl>
2020-10-21 00:00:45 +0200

    update to branch common-rl-intf :facepalm:

--- src/BeneathBeyondGame.jl
+++ src/BeneathBeyondGame.jl
@@ -1,6 +1,13 @@
 module BeneathBeyondGame
-export BBCube
+    export GameEnv, GameSpec
 
-include("game.jl")
+    include("game.jl")
+    const GameEnv = CubeEnv{5}
+    const GameSpec = CubeSpec{5}
 
+    module Training
+        using AlphaZero
+        import ..GameSpec
+        # include("params.jl")
+    end
 end
--- src/game.jl
+++ src/game.jl
@@ -1,44 +1,46 @@
-using AbstractAlgebra
+import AbstractAlgebra
 import AlphaZero.GI
 
-mutable struct BBCube{N} <: GI.AbstractGame
+struct CubeSpec{N} <: GI.AbstractGameSpec end
+
+board_shape(::Type{CubeSpec{N}}) where N = ntuple(_->2, N)
+
+mutable struct CubeEnv{N} <: GI.AbstractGameEnv
     board::BitArray{N}
     history::Vector{UInt16}
     # bb::Polymake.BeneathBeyond
 end
 
-function BBCube{N}() where N
-    history = UInt16[]
-    sizehint!(history, 2^N)
-    board = falses(ntuple(_->2, N))
-    return BBCube{N}(board, history) #, ...)
+board(g::CubeEnv) = g.board
+history(g::CubeEnv) = g.history
+
+function GI.init(::CubeSpec{N}, state=(board=falses(board_shape(CubeSpec{N})), history=UInt16[])) where N
+    sizehint!(state.history, 2^N)
+    return CubeEnv{N}(state.board, state.history)
 end
 
-BBCube{N}(state::NamedTuple) where N = BBCube{N}(state.board, state.history)
+GI.spec(::CubeEnv{N}) where N = CubeSpec{N}()
 
-board(g::BBCube) = g.board
-history(g::BBCube) = g.history
+GI.two_players(::CubeSpec) = false
 
-GI.State(::Type{BBCube{N}}) where N = NamedTuple{(:board, :history),Tuple{BitArray{N},Vector{UInt16}}}
+#####
+##### Game API
+#####
 
-GI.Action(::Type{<:BBCube}) = Int
+GI.actions(::CubeSpec{N}) where N = 1:2^N
 
-GI.two_players(::Type{<:BBCube}) = false
+GI.actions_mask(g::CubeEnv) = vec(.~(board(g)))
 
-GI.actions(::Type{BBCube{N}}) where {N} = 1:2^N
+GI.current_state(g::CubeEnv) = (board=board(g), history=history(g))
 
-GI.actions_mask(g::BBCube) = vec(.~(board(g)))
+GI.white_playing(::CubeEnv) = true
 
-GI.current_state(g::BBCube) = (board=board(g), history=history(g))
+GI.game_terminated(g::CubeEnv) = all(board(g))
 
-GI.white_playing(::Type{<:BBCube}, state) = true
+GI.white_reward(g::CubeEnv) = isempty(history(g)) ? 0. : -Float64(sum(x->x^2, history(g)))
 
-GI.game_terminated(g::BBCube) = all(board(g))
-
-GI.white_reward(g::BBCube) = isempty(history(g)) ? Int(0) : -Int(sum(x->x^2, history(g)))
-
-Base.@propagate_inbounds function Base.push!(g::BBCube{N}, n::Integer) where N
-    checkbounds(board(g), n)
+Base.@propagate_inbounds function Base.push!(g::CubeEnv, n::Integer)
+    @boundscheck checkbounds(board(g), n)
 
     g.board[n] = true
     push!(g.history, n)
@@ -47,15 +49,15 @@ Base.@propagate_inbounds function Base.push!(g::BBCube{N}, n::Integer) where N
     return g
 end
 
-GI.play!(g::BBCube, action::Integer) = push!(g, action)
+GI.play!(g::CubeEnv, action) = push!(g, action)
 
-GI.heuristic_value(g::BBCube) = isempty(history(g)) ? Int(0) : -Int(sum(history(g))) # Polymake.triangulation_size(g.bb)
+GI.heuristic_value(g::CubeEnv) = isempty(history(g)) ? Int(0) : -Int(sum(history(g))) # Polymake.triangulation_size(g.bb)
 
 #####
 ##### Machine Learning API
 #####
 
-function GI.vectorize_state(::Type{BBCube{N}}, state) where N
+function GI.vectorize_state(::CubeSpec{N}, state) where N
     res = zeros(Float32, 2^(N+1))
     @inbounds res[1:2^N] .= vec(state.board)
     @inbounds res[2^N+1:2^N+length(state.history)] .= state.history;
@@ -76,56 +78,60 @@ Base.permutedims(cidx::CartesianIndex, σ::AbstractAlgebra.Perm) = permutedims(c
 
 Base.isone(p::AbstractAlgebra.Perm) = p.d == 1:length(p.d)
 
-function action_on_gameactions(σ::AbstractAlgebra.Perm, cids, lids)
-    return [lids[permutedims(cids[a], σ)] for a in vec(lids)]
+Base.@propagate_inbounds function action_on_gameactions(σ::AbstractAlgebra.Perm, cids, lids)
+    return @inbounds [lids[permutedims(cids[a], σ)] for a in vec(lids)]
 end
 
-function action_on_gamestate(
+Base.@propagate_inbounds function action_on_gamestate(
     state,
     σ::AbstractAlgebra.Perm;
     cids=CartesianIndices(state.board),
     lids=LinearIndices(state.board)
 )
     p = action_on_gameactions(σ, cids, lids)
-    return (
-        (
-            board = permutedims(state.board, σ),
-            history = UInt16[p[h] for h in state.history]
-        ),
-        p
+    state_p = (
+        board = permutedims(state.board, σ),
+        history = UInt16[p[h] for h in state.history]
     )
+    return (state_p, p)
 end
 
-function GI.symmetries(::Type{BBCube{N}}, state) where N
+function GI.symmetries(::CubeSpec{N}, state) where N
     cids = CartesianIndices(state.board)
     lids = LinearIndices(state.board)
 
-    return [ action_on_gamestate(state, σ, cids=cids, lids=lids)
-        for σ in AbstractAlgebra.Generic.SymmetricGroup(N) if !isone(σ)
-    ]
+    return [ @inbounds action_on_gamestate(state, σ, cids=cids, lids=lids)
+        for σ in AbstractAlgebra.SymmetricGroup(N) if !isone(σ)]
 end
 
 #####
 ##### Interaction API
 #####
 
-function GI.action_string(::Type{BBCube{N}}, action) where N
-    ci = CartesianIndices(ntuple(_->2, N))[action]
+function GI.action_string(::CubeSpec{N}, action) where N
+    ci = CartesianIndices(board_shape(CubeSpec{N}))[action]
     return join(Tuple(ci) .- 1, "")
 end
 
-function GI.parse_action(::Type{BBCube{N}}, str::String) where N
+function GI.parse_action(::CubeSpec{N}, str) where N
     @assert length(str) == N
-    ci = ntuple(n -> str[n] == "0" ? 1 : 2, N)
-    return LinearIndices(ntuple(_->2, N))[ci]
+    ci = tuple(map(x-> (x == "0" ? 1 : 2), str))
+    return LinearIndices(board_shape(CubeSpec{N}))[ci]
 end
 
-function GI.read_state(::Type{BBCube{N}}) where N
+function GI.read_state(::CubeSpec{N}) where N
     throw("Not Implemented")
 end
 
-function GI.render(g::BBCube{N}; with_position_names=true, botmargin=true) where N
-    print(board(g))
+function GI.render(g::CubeEnv{N}; with_position_names=true, botmargin=true) where N
+    st = GI.current_state(g)
+    available_actions = GI.actions_mask(g)
+    k = ceil(Int, log10(2^N))
+    for action in GI.actions(GI,spec(g))
+        with_position_names && print(rpad("$action", k+2), " | ")
+        color = available_actions[action] ? crayon"bold fg:light_gray" : crayon"fg:dark_gray"
+        println(color, GI.action_string(GI.spec(g), action))
+    end
     botmargin && print("\n")
 end
 
