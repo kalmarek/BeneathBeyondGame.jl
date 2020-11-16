c8ba298518dd39932a5f6bff6aa3b673144b2d29
Lars Kastner <lars.kastner@yahoo.dk>
2020-11-16 23:38:20 +0100

    Add Training2 for CapsGame

--- src/BeneathBeyondGame.jl
+++ src/BeneathBeyondGame.jl
@@ -2,6 +2,7 @@ module BeneathBeyondGame
     export GameEnv, GameSpec
 
     include("game.jl")
+    include("caps_game.jl")
     const GameEnv = CubeEnv{5}
     const GameSpec = CubeSpec{5}
 
@@ -9,6 +10,15 @@ module BeneathBeyondGame
         using AlphaZero
         import ..GameSpec
         import ..CubeSpec
+        include("params.jl")
+    end
+    
+    const CapsGameEnv = CapsEnv{5}
+    const CapsGameSpec = CapsSpec{5}
+    module Training2
+        using AlphaZero
+        import ..CapsGameSpec
+        import ..CapsSpec
         include("params.jl")
     end
 end
